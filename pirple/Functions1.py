861f4240539a4b2d4de0ab229f800f38be86f191
meneel <59295786+meneel@users.noreply.github.com>
2020-09-29 13:19:30 +0530

    function1

---
+++ pirple/Functions1.py
@@ -0,0 +1,16 @@
+def Name():  # function for name
+    name = "Indraneel Dutta"
+    output = "My name is: " + name
+    return output
+
+
+def PhoneNumber():  # function for phone number
+    number = "7980082871"
+    output = "My Phone Number is: " + number
+    return output
+
+
+def Address():  # function for address
+    address = "98/2/1A Dr S.C Banerjee Road, Kolkata,West Bengal, India"
+    output = "My address is: " + address
+    return output
